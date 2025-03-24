50bb9ce0a4dcb098c97c335b54315f816c06d05e
Hieu Do <hieudv@carpla.vn>
2025-03-24 10:50:34 +0700

    ghep api remote_inspection_detail

--- app/api/v1/endpoints/assessment_task_status.py
+++ app/api/v1/endpoints/assessment_task_status.py
@@ -119,10 +119,12 @@ async def get_remote_inspection(assessment_id: int, invitation_code: str) -> Lis
     data = []
     for res in result:
         if res.get('status') == 'new':
+            label = "Đang chờ giám định từ xa"
             message = f"Hồ sơ này đang chờ người khác thực hiện giám định từ xa với mã: {res.get('invitation_code')}"
             btn_cancel = True
             btn_share = True
         else:
+            label = "Đã hoàn thành giám định từ xa"
             message = f"Hồ sơ này đã được giám định từ xa bởi: {res.get('name')}"
             btn_cancel = False
             btn_share = False
@@ -132,7 +134,7 @@ async def get_remote_inspection(assessment_id: int, invitation_code: str) -> Lis
             'phone': res.get('phone'),
             'invitation_code': res.get('invitation_code'),
             'status': res.get('status'),
-            'label': 'Đang chờ giám định từ xa' if res.get('status') == 'new' else 'Đã hoàn thành giám định từ xa',
+            'label': label,
             'message': message,
             'btn_cancel': btn_cancel,
             'btn_share': btn_share,
